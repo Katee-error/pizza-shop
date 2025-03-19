bb1afce8f6ac2cfd06410e69d6382e04ae00b46c
Ekaterina <kate8190758@gmail.com>
2025-03-19 15:34:14 +0400

    feat: add auth me

--- src/app/(checkout)/checkout/page.tsx
+++ src/app/(checkout)/checkout/page.tsx
@@ -1,76 +1,88 @@
-"use client";
-import { Container } from "@/components/layout";
-import { Title } from "@/components/ui";
-import { useCart } from "@/shared/hooks/use-cart";
-import { useState } from "react";
-import { FormProvider, useForm } from "react-hook-form";
-import { zodResolver } from "@hookform/resolvers/zod";
-import toast from "react-hot-toast";
-import { createOrder } from "@/app/actions";
-import { CheckoutAddressForm, CheckoutCart, CheckoutPersonalForm, CheckoutSidebar } from "@/components/shared/checkout";
-import { checkoutFormSchema, CheckoutFormValues } from "@/shared/constants/checkout-form-schema";
+'use client';
+
+import { FormProvider, useForm } from 'react-hook-form';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { createOrder } from '@/app/actions';
+import toast from 'react-hot-toast';
+import React from 'react';
+import { useSession } from 'next-auth/react';
+import { Api } from '@/shared/services/api-client';
+import { checkoutFormSchema, CheckoutFormValues} from '@/shared/constants/checkout-form-schema';
+import { useCart } from '@/shared/hooks/use-cart';
+import { Container } from '@/components/layout';
+import { Title } from '@/components/ui';
+import { CheckoutAddressForm, CheckoutCart, CheckoutPersonalForm, CheckoutSidebar } from '@/components/shared/checkout';
 
 export default function CheckoutPage() {
-  const { totalAmount, updateItemQuantity, items, removeCartItem, loading } =
-    useCart();
-  const [submitting, setSubmitting] = useState(false);
-
-  const onClickCountButton = (
-    id: number,
-    quantity: number,
-    type: "plus" | "minus"
-  ) => {
-    const newQuantity = type === "plus" ? quantity + 1 : quantity - 1;
-    updateItemQuantity(id, newQuantity);
-  };
+  const [submitting, setSubmitting] = React.useState(false);
+  const { totalAmount, updateItemQuantity, items, removeCartItem, loading } = useCart();
+  const { data: session } = useSession();
 
   const form = useForm<CheckoutFormValues>({
     resolver: zodResolver(checkoutFormSchema),
     defaultValues: {
-      email: "",
-      firstName: "",
-      lastName: "",
-      phone: "",
-      address: "",
-      comment: "",
+      email: '',
+      firstName: '',
+      lastName: '',
+      phone: '',
+      address: '',
+      comment: '',
     },
   });
 
+  React.useEffect(() => {
+
+    async function fetchUserInfo() {
+      const data = await Api.auth.getMe();
+      const [firstName, lastName] = data.fullName.split(' ');
+
+      form.setValue('firstName', firstName);
+      form.setValue('lastName', lastName);
+      form.setValue('email', data.email);
+    }
+
+    if (session) {
+      fetchUserInfo();
+    }
+  }, [session]);
 
   const onSubmit = async (data: CheckoutFormValues) => {
-    console.log(data)
     try {
       setSubmitting(true);
 
-      const url = await createOrder(data)
-      toast.success('Заказ успешно оформлен! 📝 Переход на оплату... ', {
-        icon: '✅',
-      });
+      const response = await createOrder(data);
 
-      if (typeof url === "string" && url) {
-        location.href = url;
+      if (response.success && response.paymentUrl) {
+        toast.success('Заказ успешно оформлен! 📝 Переход на оплату...', {
+          icon: '✅',
+        });
+
+        location.href = response.paymentUrl;
+      } else {
+        throw new Error('Не удалось получить ссылку на оплату');
       }
-
-    } catch (error) {
+    } catch (err) {
+      console.error(err);
       setSubmitting(false);
-      console.log(error);
-      toast.error("Не удалось создать заказ", {
-        icon: "❌",
+      toast.error('Не удалось создать заказ', {
+        icon: '❌',
       });
     }
-  };
+};
 
-  console.log(form.formState.errors);
+  const onClickCountButton = (id: number, quantity: number, type: 'plus' | 'minus') => {
+    const newQuantity = type === 'plus' ? quantity + 1 : quantity - 1;
+    updateItemQuantity(id, newQuantity);
+  };
 
   return (
     <Container className="mt-10">
-      <Title
-        text="Оформление заказа"
-        className="font-extrabold mb-8 text-[36px]"
-      />
+      <Title text="Оформление заказа" className="font-extrabold mb-8 text-[36px]" />
+
       <FormProvider {...form}>
-        <form onSubmit={form.handleSubmit(onSubmit)} >
+        <form onSubmit={form.handleSubmit(onSubmit)}>
           <div className="flex gap-10">
+            {/* Левая часть */}
             <div className="flex flex-col gap-10 flex-1 mb-20">
               <CheckoutCart
                 onClickCountButton={onClickCountButton}
@@ -79,20 +91,14 @@ export default function CheckoutPage() {
                 loading={loading}
               />
 
-              <CheckoutPersonalForm
-                className={loading ? "opacity-40 pointer-events-none" : ""}
-              />
+              <CheckoutPersonalForm className={loading ? 'opacity-40 pointer-events-none' : ''} />
 
-              <CheckoutAddressForm
-                className={loading ? "opacity-40 pointer-events-none" : ""}
-              />
+              <CheckoutAddressForm className={loading ? 'opacity-40 pointer-events-none' : ''} />
             </div>
 
+            {/* Правая часть */}
             <div className="w-[450px]">
-              <CheckoutSidebar
-                totalAmount={totalAmount}
-                loading={loading || submitting}
-              />
+              <CheckoutSidebar totalAmount={totalAmount} loading={loading || submitting} />
             </div>
           </div>
         </form>
--- src/app/(home)/layout.tsx
+++ src/app/(home)/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Header } from "@/components/layout";
+import { Suspense } from "react";
 
 export const metadata: Metadata = {
   title: "E-commerce shop",
@@ -15,7 +16,9 @@ export default function HomeLayout({
 }>) {
   return (
     <main className="min-h-screen">
-      <Header />
+      <Suspense>
+        <Header />
+      </Suspense>
       {children}
       {modal}
     </main>
